fcb5e0093313f40f152447d11334b679e8bc576b
qalle2 <qalle85@gmail.com>
2021-03-15 05:47:44 +0200

    foo

--- nounclass_test.py
+++ nounclass_test.py
@@ -4,52 +4,50 @@ import re
 import sys
 import nounclass
 
-RE_TWO_CLASSES = re.compile(
+# a noun with (at least) one declension 0...49
+RE_ONE_DECLENSION = re.compile(
     r"<s>([^<]+)</s>"
-    r".*>([0-9]+)</tn>"
-    r".*>([0-9]+)</tn>"
+    r".*>([1-4]?[0-9])</tn>"
 )
 
-RE_ONE_CLASS = re.compile(
+# a noun with two declensions 0...49
+RE_TWO_DECLENSIONS = re.compile(
     r"<s>([^<]+)</s>"
-    r".*>([0-9]+)</tn>"
+    r".*>([1-4]?[0-9])</tn>"
+    r".*>([1-4]?[0-9])</tn>"
 )
 
-def get_words():
-    """Generate (word, (conjugation1, ...)) from stdin."""
+def get_nouns():
+    """Generate (word, {declension1, ...}) from stdin."""
 
     for line in sys.stdin:
-        match = RE_TWO_CLASSES.search(line)
-        if match is not None:
-            conjugations = (int(g) for g in match.group(2, 3))
-            yield (match.group(1), tuple(sorted(conjugations)))
-        else:
-            match = RE_ONE_CLASS.search(line)
-            if match is not None:
-                conjugation = int(match.group(2))
-                yield (match.group(1), (conjugation,))
+        match1 = RE_ONE_DECLENSION.search(line)
+        if match1 is not None:
+            match2 = RE_TWO_DECLENSIONS.search(line)
+            if match2 is not None:
+                yield (match2.group(1), {int(g) for g in match2.group(2, 3)})
+            else:
+                yield (match1.group(1), {int(match1.group(2))})
 
 def main():
     nounCount = 0
     errorCount = 0
 
-    for (word, conjus) in get_words():
-        if 1 <= conjus[0] <= 49 and word == word.lower():
-            conjus = tuple(c for c in conjus if 1 <= c <= 49)
-            detectedConjus = nounclass.get_noun_class(word)
-            conjusStr = "/".join(str(c) for c in conjus)
-            detectedConjusStr = "/".join(str(c) for c in detectedConjus)
+    for (word, correctDeclensions) in get_nouns():
+        detectedDeclensions = nounclass.get_noun_class(word)
+        correctDeclStr  = "/".join(str(c) for c in sorted(correctDeclensions))
+        detectedDeclStr = "/".join(str(c) for c in sorted(detectedDeclensions))
 
-            if not all(c in detectedConjus for c in conjus):
-                print(
-                    f"Error: '{word}': expected declension {conjusStr}, got {detectedConjusStr}",
-                    file=sys.stderr
-                )
-                errorCount += 1
-            elif len(conjus) < len(detectedConjus):
-                print(f"Note: '{word}': expected {conjusStr}, got {detectedConjusStr}")
+        if not correctDeclensions.issubset(detectedDeclensions):
+            print(
+                f"Error: '{word}': expected declension {correctDeclStr}, got {detectedDeclStr}",
+                file=sys.stderr
+            )
+            errorCount += 1
+        elif len(correctDeclensions) < len(detectedDeclensions):
+            print(f"Note: '{word}': expected {correctDeclStr:>2}, got {detectedDeclStr}")
 
-            nounCount += 1
+        nounCount += 1
 
     print(f"Nouns checked: {nounCount}, errors: {errorCount}")
     print("Inspect the notes above to see if incorrect declensions were returned too.")
