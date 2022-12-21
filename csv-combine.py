12b029dcd6c416efb883c84630398e16f62c5979
qalle2 <qalle85@gmail.com>
2022-12-22 01:13:01 +0200

    tidy up

--- csv-combine.py
+++ csv-combine.py
@@ -1,25 +1,31 @@
 import sys
-import util
 
-if len(sys.argv) < 2:
-    sys.exit(
-        "Arguments: one or more CSV files. For each distinct word, print a "
-        "CSV line with all declensions/conjugations occurring with that word "
-        "in the files."
-    )
+def read_lines(filename):
+    with open(filename, "rt", encoding="utf8") as handle:
+        handle.seek(0)
+        yield from (l.rstrip("\n") for l in handle)
 
-conjugationsByWord = {}  # word: set of conjugations
+def main():
+    if len(sys.argv) < 2:
+        sys.exit(
+            "Arguments: one or more CSV files. For each distinct word, print "
+            "a CSV line with all declensions/conjugations occurring with that "
+            "word in the files."
+        )
+    filenames = sys.argv[1:]
 
-# read files
-for file_ in sys.argv[1:]:
-    for line in util.read_lines(file_):
-        fields = line.split(",")
-        word = fields[0]
-        conjugations = {int(v, 10) for v in fields[1:]}
-        conjugationsByWord.setdefault(word, set()).update(conjugations)
+    conjugationsByWord = {}  # {word: set of conjugations, ...}
 
-# print results
-for word in conjugationsByWord:
-    print(",".join(
-        [word] + [str(c) for c in sorted(conjugationsByWord[word])]
-    ))
+    for filename in filenames:
+        for line in read_lines(filename):
+            fields = line.split(",")
+            word = fields[0]
+            conjugations = {int(v) for v in fields[1:]}
+            conjugationsByWord.setdefault(word, set()).update(conjugations)
+
+    for word in sorted(conjugationsByWord):
+        print(",".join(
+            [word] + [str(c) for c in sorted(conjugationsByWord[word])]
+        ))
+
+main()
